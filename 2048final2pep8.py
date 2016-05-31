f58171ff210d8e20be03a3a0ed0ae5d3a7efdd12
Shevah <shevah92@gmail.com>
2016-05-31 14:31:40 +0200

    tile 16 is now blue

--- 2048final2pep8.py
+++ 2048final2pep8.py
@@ -206,6 +206,9 @@ def color_numbers(screen, i, j, numbers):
     elif list_of_numbers[i][j] == 8:
         curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
         screen.addstr((i+1)*4, (j+1)*15, numbers, curses.color_pair(3))
+    elif list_of_numbers[i][j] == 16:
+        curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        screen.addstr((i+1)*4, (j+1)*15, numbers, curses.color_pair(4))
     else:
         screen.addstr((i+1)*4, (j+1)*15, numbers)
 
